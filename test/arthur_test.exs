ab307bf58d6fee675e6507da28f4b6407bbb3a1d
Harlan T Wood <code+harlan@harlantwood.net>
2018-03-10 02:11:14 -1000

    ...

--- test/arthur_test.exs
+++ test/arthur_test.exs
@@ -2,7 +2,7 @@ defmodule ArthurTest do
   use ExUnit.Case
   doctest Arthur
 
-  test "greets the world" do
-    assert Arthur.hello() == :world
-  end
+  #  test "greets the world" do
+  #    assert Arthur.hello() == :world
+  #  end
 end
